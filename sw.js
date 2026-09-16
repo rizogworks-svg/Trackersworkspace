acd4b6aa8746510f29282670aa566a264c6b3942
Rizog <rizogworks@gmail.com>
2026-09-16 22:46:37 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'trackers-workspace-20260916-clean1';
+const CACHE_NAME = 'trackers-workspace-20260916-clean2';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
   './assets/css/app.css',
+  './assets/css/layout-fix.css',
   './assets/js/storage-guard.js',
   './assets/js/config.js',
   './assets/js/cloud.js',
